fc9ab41b107cd20a6b03e56b13fa706ed6085bbb
Msksgm <msksgm3124@gmail.com>
2024-03-31 07:31:03 +0900

    chore: kotest のライブラリを Arrow が利用しているバージョンに更新

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,6 +11,12 @@ repositories {
 
 dependencies {
     testImplementation(kotlin("test"))
+
+    // kotest の依存関係
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.1") // KotestのJUnit5ランナー
+    testImplementation("io.kotest:kotest-assertions-core:5.8.1") // Kotestのアサーションライブラリ
+    testImplementation("io.kotest:kotest-property:5.8.1") // Kotestのプロパティベースのテスト
+    testImplementation("io.kotest:kotest-framework-engine:5.8.1") // Kotestのプロパティベースのテスト
 }
 
 tasks.test {
@@ -19,4 +25,8 @@ tasks.test {
 
 kotlin {
     jvmToolchain(8)
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
 }
