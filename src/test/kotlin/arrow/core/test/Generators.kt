ac435107398434d9ee052833506a02e13b857988
Msksgm <msksgm3124@gmail.com>
2024-04-18 07:17:29 +0900

    test: 「terable.toNonEmptyListOrNull should round trip」を写経。NonEmptyList の実装はま test: 「terable.toNonEmptyListOrNull should round trip」を写経。NonEmptyList の実装はまだ

--- src/test/kotlin/arrow/core/test/Generators.kt
+++ src/test/kotlin/arrow/core/test/Generators.kt
@@ -1,9 +1,15 @@
 package arrow.core.test
 
 import arrow.core.Either
+import arrow.core.NonEmptyList
 import io.kotest.property.Arb
+import io.kotest.property.arbitrary.bind
 import io.kotest.property.arbitrary.choice
+import io.kotest.property.arbitrary.list
 import io.kotest.property.arbitrary.map
+
+fun <A> Arb.Companion.nonEmptyList(arb: Arb<A>, range: IntRange = 0..100): Arb<NonEmptyList<A>> =
+    Arb.bind(arb, Arb.list(arb, range), ::NonEmptyList)
 
 fun <E, A> Arb.Companion.either(arbE: Arb<E>, arbA: Arb<A>): Arb<Either<E, A>> {
     val arbLeft = arbE.map { Either.Left(it) }
